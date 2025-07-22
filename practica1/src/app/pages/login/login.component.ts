2b1b8800f09d25b2c5b1b2a9bdf492d0d41f9e42
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-07-21 20:19:55 -0500

    xd

---
+++ practica1/src/app/pages/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.form = fb.group({
+      usuario: ['', Validators.required],
+      contrasenia: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    console.log('=>', this.form.getRawValue());
+  }
+
+  iniciarSesion() {
+    console.log('=>', this.form.getRawValue());
+    console.log('usuario =>', this.form.controls['usuario'].value);
+    console.log('contrasenia =>', this.form.controls['contrasenia'].value);
+    console.log('->', this.form.valid)
+  }
+}
